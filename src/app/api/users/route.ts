c7bd76ff524d509389fec85aaf8eb14c8eabb4da
Muzzammil Hussain <muzzammil5@gmail.com>
2024-12-24 11:31:56 +0500

    delete api folder

--- src/app/api/users/route.ts
+++
@@ -1,9 +0,0 @@
-// import { NextResponse } from "next/server";
-// import { sql } from "@vercel/postgres"
-
-
-
-// export async function GET(request: Request) {
-//     const users = await sql'SELE * FROM USERS;';
-//     return NextResponse.json(users);
-// }
