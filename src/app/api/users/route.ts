7efe97e2244b641600681a3f0288aaa779000359
Muzzammil Hussain <muzzammil5@gmail.com>
2024-12-24 11:28:22 +0500

    changes

---
+++ src/app/api/users/route.ts
@@ -0,0 +1,9 @@
+// import { NextResponse } from "next/server";
+// import { sql } from "@vercel/postgres"
+
+
+
+// export async function GET(request: Request) {
+//     const users = await sql'SELE * FROM USERS;';
+//     return NextResponse.json(users);
+// }
